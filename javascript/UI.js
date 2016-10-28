a5e4206d9d45d44895b2c3f9f5aecef1fce38e58
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-10-28 11:49:16 -0400

    Created elements similar to the html in order to have my own article and buttons in order to carry out my submit function and store the input value.

--- javascript/UI.js
+++ javascript/UI.js
@@ -1,4 +1,4 @@
 (function() {
     window.todo = window.todo || {};
-
+    
 })();
--- javascript/data.js
+++ javascript/data.js
@@ -1,4 +1,30 @@
 (function() {
+
     window.todo = window.todo || {};
+
+    function addToDo(text) {
+
+        var addLi = document.createElement('li');
+        addLi.innerText = text
+        addLi.classList.add('.new-to-do');
+        document.querySelector('ul').appendChild(addLi);
+
+        var addArticle = document.createElement('article');
+        document.querySelector('li').appendChild(addArticle);
+
+        var addButton = document.createElement('button');
+        addButton.classList.add('.check');
+        document.querySelector('article').appendChild(addButton);
+
+        var addP = document.createElement('p');
+        document.querySelector('article').appendChild(addP);
+
+        var addXButton = document.createElement('button');
+        addXButton.classList.add('.delete');
+        document.querySelector('article').appendChild(addXButton);
+
+
+    }
     
+
 })();
