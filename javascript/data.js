dc1f544f6a7f97495103aa43a968acb3127a1beb
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-10-28 16:23:51 -0400

    Created the click eventListener for the click to mark complete.

--- javascript/data.js
+++ javascript/data.js
@@ -1,27 +1,20 @@
 (function() {
-
-    var toDoArray = [];
     window.todo = window.todo || {};
 
+    var toDoArray = [];
     function addToDo(text) {
-
         var addLi = document.createElement('li');
-
         var addArticle = document.createElement('article');
-
         var addButton = document.createElement('button');
         addButton.classList.add('check');
         addArticle.appendChild(addButton);
-
         var addP = document.createElement('p');
         addP.innerText = text;
         addArticle.appendChild(addP);
-
         var addXButton = document.createElement('button');
         addXButton.classList.add('delete');
         addXButton.innerText = 'X';
         addArticle.appendChild(addXButton);
-
         addLi.appendChild(addArticle)
         document.querySelector('ul').appendChild(addLi);
     }
@@ -29,34 +22,28 @@
     // Everything below this is code for the submit
 
     document
-    .querySelector('form')
-    .addEventListener('submit', function submit(eventObj) {
-    eventObj.preventDefault();
+        .querySelector('form')
+        .addEventListener('submit', function submit(eventObj) {
+            eventObj.preventDefault();
 
-      var toDoSubmission = document.querySelector('.new-todo').value;
+            var toDoSubmission = document.querySelector('.new-todo').value;
 
-      var toDoList = {
-        text: toDoSubmission,
-        time: Date.now()
-      };
+            var toDoList = {
+                text: toDoSubmission,
+                time: Date.now()
+            };
+            toDoArray.push(toDoList);
+            console.log(toDoArray);
+            addToDo(toDoSubmission);
+        });
 
-      toDoArray.push(toDoList);
-      console.log(toDoArray);
+        document
+        .querySelector('ul')
+        .addEventListener('click', function complete(eventObj) {
+            console.log('in complete');
+            eventObj.target.classList.add('completed');
 
-
-
-
-
-
-
-
-
-
-        addToDo(toDoSubmission);
-
-    });
-
-
+        });
 
 
 
