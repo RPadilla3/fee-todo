3604d088d3ad6a899d62fc671378f56afcf0a764
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-10-30 23:57:06 -0400

    Created two separate folders for my js files, one for UI and the other for the data.

--- javascript/UI.js
+++ javascript/UI.js
@@ -1,4 +1,45 @@
 (function() {
-    window.todo = window.todo || {};
-    
+
+  function addToDo(text) {
+      var addLi = document.createElement('li');
+      var addArticle = document.createElement('article');
+      var addButton = document.createElement('button');
+      addButton.classList.add('check');
+      addArticle.appendChild(addButton);
+      var addP = document.createElement('p');
+      addP.innerText = text;
+      addArticle.appendChild(addP);
+      var addXButton = document.createElement('button');
+      addXButton.classList.add('delete');
+      addXButton.innerText = 'X';
+      addArticle.appendChild(addXButton);
+      addLi.appendChild(addArticle)
+      document.querySelector('ul').appendChild(addLi);
+  }
+
+
+    document
+        .querySelector('form')
+        .addEventListener('submit', function submit(eventObj) {
+            eventObj.preventDefault();
+
+            var toDoSubmission = document.querySelector('.new-todo').value;
+
+            var toDoList = {
+                text: toDoSubmission,
+                time: Date.now()
+            };
+            toDoArray.push(toDoList);
+            console.log(toDoArray);
+            addToDo(toDoSubmission);
+        });
+
+        document
+        .querySelector('article')
+        .addEventListener('click', function complete(eventObj) {
+            console.log('in complete');
+            eventObj.target.classList.add('completed');
+
+        });
+  window.todo = window.todo || {};
 })();
--- javascript/data.js
+++ javascript/data.js
@@ -1,41 +1,39 @@
 (function() {
-    window.todo = window.todo || {};
-
-    var toDoArray = [];
-    function addToDo(text) {
-        var addLi = document.createElement('li');
-        var addArticle = document.createElement('article');
-        var addButton = document.createElement('button');
-        addButton.classList.add('check');
-        addArticle.appendChild(addButton);
-        var addP = document.createElement('p');
-        addP.innerText = text;
-        addArticle.appendChild(addP);
-        var addXButton = document.createElement('button');
-        addXButton.classList.add('delete');
-        addXButton.innerText = 'X';
-        addArticle.appendChild(addXButton);
-        addLi.appendChild(addArticle)
-        document.querySelector('ul').appendChild(addLi);
-    }
+      var toDoArray = [];
+    // function addToDo(text) {
+    //     var addLi = document.createElement('li');
+    //     var addArticle = document.createElement('article');
+    //     var addButton = document.createElement('button');
+    //     addButton.classList.add('check');
+    //     addArticle.appendChild(addButton);
+    //     var addP = document.createElement('p');
+    //     addP.innerText = text;
+    //     addArticle.appendChild(addP);
+    //     var addXButton = document.createElement('button');
+    //     addXButton.classList.add('delete');
+    //     addXButton.innerText = 'X';
+    //     addArticle.appendChild(addXButton);
+    //     addLi.appendChild(addArticle)
+    //     document.querySelector('ul').appendChild(addLi);
+    // }
 
     // Everything below this is code for the submit
 
-    document
-        .querySelector('form')
-        .addEventListener('submit', function submit(eventObj) {
-            eventObj.preventDefault();
-
-            var toDoSubmission = document.querySelector('.new-todo').value;
-
-            var toDoList = {
-                text: toDoSubmission,
-                time: Date.now()
-            };
-            toDoArray.push(toDoList);
-            console.log(toDoArray);
-            addToDo(toDoSubmission);
-        });
+    // document
+    //     .querySelector('form')
+    //     .addEventListener('submit', function submit(eventObj) {
+    //         eventObj.preventDefault();
+    //
+    //         var toDoSubmission = document.querySelector('.new-todo').value;
+    //
+    //         var toDoList = {
+    //             text: toDoSubmission,
+    //             time: Date.now()
+    //         };
+    //         toDoArray.push(toDoList);
+    //         console.log(toDoArray);
+    //         addToDo(toDoSubmission);
+    //     });
 
         // document
         // .querySelector('ul')
